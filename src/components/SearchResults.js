796f1ac0a17a7965201b49c499af93afb9a37b5e
sy4sylvia <sguo713@gmail.com>
2022-11-01 09:49:55 -0400

    feature: add unfiltered search results page with mock data

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,18 +1,109 @@
-import React from 'react';
-import { Pagination } from 'antd';
+import React, { useState } from 'react';
+import {Typography, Layout, Menu, Row, Col, Pagination} from 'antd';
+import { DesktopOutlined, CloudSyncOutlined, HomeOutlined,} from '@ant-design/icons';
 
-import 'antd/dist/antd.css';
+import { FURNITURE, OFFICE, TECHNOLOGY } from './CategoryItems'
 
-const SearchResults = () => {
+const _ = require('lodash');
 
-    return (
-        <div>
+const { Title } = Typography;
+const { Header, Content, Sider } = Layout;
 
-            <Pagination defaultCurrent={6} total={500} />
-        </div>
-    )
+const style = {
+    background: '#0092ff',
+    padding: '8px 0',
+};
 
-
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
 }
 
+const furnitureChildren = [];
+const officeChildren = [];
+const techChildren = [];
+
+_.forEach(FURNITURE, function(obj) {
+    furnitureChildren.push(getItem(obj.label, obj.key));
+});
+
+_.forEach(OFFICE, function(obj) {
+    officeChildren.push(getItem(obj.label, obj.key));
+});
+
+_.forEach(TECHNOLOGY, function(obj) {
+    techChildren.push(getItem(obj.label, obj.key));
+});
+
+const items = [
+    getItem('Furnitures', 'furniture', <HomeOutlined />, furnitureChildren),
+    getItem('Office Supplies', 'office', <DesktopOutlined />, officeChildren),
+    getItem('Technology', 'technology', <CloudSyncOutlined />, techChildren),
+];
+
+const SearchResults = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="logo" />
+                {/*TODO: choose the color of the title and move down a bit */}
+                <Title level={5} type = 'success'> Category </Title>
+                <Menu
+                    theme='dark' defaultSelectedKeys={['furniture']} mode="inline" items={items} />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Title> Search Results </Title>
+
+                    <div
+                        className="site-layout-background"
+                        style={{padding: '10px 120px'}} >
+
+                        <Row
+                            gutter={{ xs: 8,  sm: 16, md: 24, lg: 32,}}
+                            style={{padding: '120px 120px'}}
+                        >
+                            <Col className="gutter-row" span={6}>
+                                <div style={style}>product-1</div>
+                            </Col>
+                            <Col className="gutter-row" span={6}>
+                                <div style={style}>product-2</div>
+                            </Col>
+                            <Col className="gutter-row" span={6}>
+                                <div style={style}>product-3</div>
+                            </Col>
+                            <Col className="gutter-row" span={6}>
+                                <div style={style}>product-4</div>
+                            </Col>
+                        </Row>
+                        <Pagination
+                            defaultCurrent={6} total={500}
+                            style={{padding: '120px 0'}}
+                        />
+                    </div>
+
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
 export default SearchResults;
